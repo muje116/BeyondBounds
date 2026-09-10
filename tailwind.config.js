33ea8d3c45cd68f45fba973af8ccba1c13b33d27
James Simbi <james@sparcsystems.africa>
2026-09-10 23:49:39 +0200

    Improve blog and team readability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  darkMode: 'class',
   content: ['./themes/beyondbounds/**/*.htm','./themes/beyondbounds/**/*.html','./plugins/beyondbounds/**/*.htm'],
   theme: {
     extend: {
